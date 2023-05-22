054ff2ae722994a58dd4e527c477e9b10598e805
Joseph Adamson <josephowenadamson@gmail.com>
2023-05-22 16:46:13 +0100

    cli accepts multiple file args, prints them to the terminal

--- src/args.rs
+++ src/args.rs
@@ -1,10 +1,15 @@
-use clap::Parser;
+use clap::{ Parser };
 
 #[derive(Debug, Default, Parser)]
-#[command(author="Joe Adamson", version, about)]
+#[command(author="Joe Adamson")] 
+#[command(version = "1.0")]
 /// Create cool ASCII images from jpg, png and gif files
 pub struct AsciiArgs {
-    /// file(s) to be converted into ascii art
-    #[arg(short, long)]
-    pub files: String,
+    /// File(s) to be converted into ascii art
+    #[arg(long, short, num_args = 1.., value_delimiter = ' ')]
+    pub files: Vec<String>,
+
+    /// Save ascii output to a .txt file at the given file path
+    #[arg(long, short)]
+    pub output_path: Option<String>
 }
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,14 @@ mod args;
 
 use args::AsciiArgs;
 use clap::Parser;
+use convert_img::print_img_to_console;
 
 fn main() {
     let args: AsciiArgs = AsciiArgs::parse();
-    println!("{:?}", args);
+
+    if args.output_path.is_none() {
+        for file_path in args.files {
+            print_img_to_console(file_path);
+        }
+    }
 }
